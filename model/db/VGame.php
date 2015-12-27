8dfc15332a097d2ec5a9e1f004490802bf3be411
Thoronir42 <Thoronir42@Gmail.com>
2015-12-27 17:56:13 +0100

    View obrazky now properly shows multiple images for games

--- model/db/VGame.php
+++ model/db/VGame.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace model\db;
 
 /**
@@ -6,9 +7,22 @@ namespace model\db;
  *
  * @author Stepan
  */
-class VGame extends Game{
-	
+class VGame extends Game {
+
 	var $picture_path;
 	var $picture_description;
-	
+
+	public function __construct() {
+		parent::__construct();
+		$this->misc['images'] = [];
+	}
+
+	/**
+	 * 
+	 * @param Image $i
+	 */
+	public function addImage($i) {
+		$this->misc['images'][] = $i;
+	}
+
 }
